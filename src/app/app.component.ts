a515f7a59e38f3cb1520374ff03f5662dd001d42
Joel Gallardo Manjarrez <joel.gallardo@coppel.com>
2023-06-09 09:02:23 -0700

    form create person, servies get paises and estados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EstadosService } from './services/estados/estados.service';
+import { PaisesService } from './services/paises/paises.service';
+import { PersonaService } from './services/persona/persona.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [ './app.component.css' ],
 })
-export class AppComponent {
-  title = 'frontend_persons_CRUD';
+export class AppComponent implements OnInit {
+  personaForm!: FormGroup;
+  paises: any;
+  estados: any;
+
+  constructor(
+    public fb: FormBuilder,
+    public estadoService: EstadosService,
+    public paisesServices: PaisesService,
+    public personaService: PersonaService
+  ) { }
+
+  ngOnInit(): void {
+    this.personaForm = this.fb.group({
+      nombre: [ '', Validators.required ],
+      apellido: [ '', Validators.required ],
+      edad: [ '', Validators.required ],
+      pais: [ '', Validators.required ],
+      estado: [ '', Validators.required ]
+    });
+
+    this.paisesServices.getAllPaises().subscribe(
+      (resp) => {
+        this.paises = resp
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+
+  }
+
+  guardar(): void { }
+
+  cargarEstadosPorPaisId(event: number) {
+    this.estadoService.getAllEstadosByPais(event).subscribe(resp => {
+      this.estados = resp;
+    },
+      error => {
+        console.error(error)
+      }
+    )
+  }
 }
